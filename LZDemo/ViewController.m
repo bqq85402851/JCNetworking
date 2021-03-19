dc586d50ad97a8315175d134bfe59640c741ad15
毕亚强 <bqq8540@163.com>
2021-03-19 17:34:34 +0800

    first commit

---
+++ LZDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LZDemo
+//
+//  Created by 毕小强 on 2021/3/19.
+//  Copyright © 2021 毕小强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ LZDemo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  LZDemo
+//
+//  Created by 毕小强 on 2021/3/19.
+//  Copyright © 2021 毕小强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
