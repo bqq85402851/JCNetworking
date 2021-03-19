78ae37b1935ad559d190cf514a80f71ef7d75cff
毕亚强 <bqq8540@163.com>
2021-03-19 17:23:04 +0800

    init project

---
+++ LZDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  LZDemo
+//
+//  Created by 毕小强 on 2021/3/19.
+//  Copyright © 2021 毕小强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ LZDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LZDemo
+//
+//  Created by 毕小强 on 2021/3/19.
+//  Copyright © 2021 毕小强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
